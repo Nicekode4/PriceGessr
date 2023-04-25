a3033ec9018ce69e40011963a0e0803a7c9c615e
Philip Thomsen <philip-th@live.dk>
2023-04-25 09:48:28 +0200

    🐛 fix(sw.js): rename urlsToCache variable to assets for clarity and consistency

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
 
 // De filer du vil cache
-const urlsToCache = [
+const assets = [
   '/',
   '/index.html',
   '/fallback.html',
@@ -17,7 +17,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return cache.addAll(urlsToCache)
+        return cache.addAll(assets)
           .catch(error => {
             console.error('Failed to cache', error);
           });
